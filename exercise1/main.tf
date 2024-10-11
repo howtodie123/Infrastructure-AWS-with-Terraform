2b8462ba66defe9892ee14fc3423d2a97afa3ae7
howtodie123 <nguyenletienphat123123@gmail.com>
2024-10-11 23:29:19 +0700

    Create Test for VPC and test a lot of things

--- exercise1/main.tf
+++ exercise1/main.tf
@@ -2,15 +2,15 @@ terraform {
   required_providers {
     aws = {
       source = "hashicorp/aws"
-      version = "5.71.0"
+      version = "5.70.0"
     }
   }
 }
 
 provider "aws" {
-  region = "us-east-1" # this region is the UIT provided region , can change to any region
-  access_key = ""
-  secret_key = ""
+  region = "us-east-1" 
+  access_key = "AKIA2UC3DTC6LDG2ULVU"
+  secret_key = "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"
 }
 
 # resource "aws_instance" "Group7" {
@@ -20,40 +20,71 @@ provider "aws" {
 
 module "VPC" {
   source                  = "./modules/VPC"
-  vpc_cidr                = "10.0.0.0/16"
-  public_subnet_cidr      = "10.0.1.0/24"
-  private_subnet_cidr     = "10.0.2.0/24"
-  availability_zone       = "us-east-1a"
+  vpc_cidr                = var.vpc_cidr #"10.0.0.0/16"
+  public_subnet_cidr      = var.public_subnet_cidr #"10.0.1.0/24"
+  private_subnet_cidr     = var.private_subnet_cidr #"10.0.2.0/24"
+  availability_zone       = var.availability_zone #"us-east-1a"
 }
+# resource "aws_vpc" "main_vpc" {
+#   cidr_block = var.vpc_cidr
+#   enable_dns_support = true
+#   enable_dns_hostnames = true
+#   instance_tenancy     = "default"
 
-module "NAT" {
-  source                  = "./modules/NAT"
-  public_subnet_id        = module.VPC.public_subnet_id
+#   tags = {
+#     Name = "VPC group 7"
+#     Environment = "test"
+#   }
+# }
+# module "NAT" {
+#   source                  = "./modules/NAT"
+#   public_subnet_id        = module.VPC.public_subnet_id
 
-}
+# }
 
-module "Route_Table" {
-  source                  = "./modules/Route_Table"
-  vpc_id                  = module.VPC.vpc_id
-  gateway_id              = module.VPC.internet_gateway_id
-  public_subnet_id        = module.VPC.public_subnet_id
-  nat_gateway_id          = module.NAT.nat_gateway_id
-  private_subnet_id       = module.VPC.private_subnet_id  
-}
+# module "Route_Table" {
+#   source                  = "./modules/Route_Table"
+#   vpc_id                  = module.VPC.vpc_id
+#   gateway_id              = module.VPC.internet_gateway_id
+#   public_subnet_id        = module.VPC.public_subnet_id
+#   nat_gateway_id          = module.NAT.nat_gateway_id
+#   private_subnet_id       = module.VPC.private_subnet_id  
+# }
 
-module "security_group" {
-  source                  = "./modules/Security_Group"
-  vpc_id                  = module.VPC.vpc_id
-  allowed_ip              = "0.0.0.0/0" 
-}
+# module "security_group" {
+#   source                  = "./modules/Security_Group"
+#   vpc_id                  = module.VPC.vpc_id
+#   allowed_ip              = "0.0.0.0/0" 
+# }
 
 
-module "ec2" {
-  source                  = "./modules/EC2"
-  ami                     = "ami-005fc0f236362e99f"
-  instance_type           = "t2.micro"
-  public_subnet_id        = module.VPC.public_subnet_id
-  private_subnet_id       = module.VPC.private_subnet_id
-  public_security_group   = module.security_group.public_security_group_id
-  private_security_group  = module.security_group.private_security_group_id
-}
+# module "ec2" {
+#   source                  = "./modules/EC2"
+#   ami                     = "ami-005fc0f236362e99f"
+#   instance_type           = "t2.micro"
+#   public_subnet_id        = module.VPC.public_subnet_id
+#   private_subnet_id       = module.VPC.private_subnet_id
+#   public_security_group   = module.security_group.public_security_group_id
+#   private_security_group  = module.security_group.private_security_group_id
+# }
+
+# main.tf
+
+# variable "repository_name" {
+#   type = string
+#   validation {
+#     condition = length(var.repository_name) <= 100
+#     error_message = "The repository name must be less than or equal to 100 characters."
+#   }
+# }
+
+# variable "kms_key_id" {
+#   type = string
+#   default = ""
+# }
+
+# resource "aws_codecommit_repository" "test" {
+#   repository_name = format("repo-%s", var.repository_name)
+#   description = "Test repository."
+#   kms_key_id = var.kms_key_id != "" ? var.kms_key_id : null
+# }
---
+++ exercise1/variable.tf
@@ -0,0 +1,23 @@
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  type        = string
+  default     = "10.0.0.0/16"  # Giá trị mặc định, có thể thay đổi
+}
+
+variable "public_subnet_cidr" {
+  description = "CIDR block for the public subnet"
+  type        = string
+  default     = "10.0.1.0/24"  # Giá trị mặc định, có thể thay đổi
+}
+
+variable "private_subnet_cidr" {
+  description = "CIDR block for the private subnet"
+  type        = string
+  default     = "10.0.2.0/24"  # Giá trị mặc định, có thể thay đổi
+}
+
+variable "availability_zone" {
+  description = "The availability zone for the subnets"
+  type        = string
+  default     = "us-west-2a"  # Giá trị mặc định, có thể thay đổi
+}

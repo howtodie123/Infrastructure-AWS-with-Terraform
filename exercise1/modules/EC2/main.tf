a91e1a2be2befc1c78341bbebd5e598f1455a839
howtodie123 <nguyenletienphat123123@gmail.com>
2024-10-11 16:59:27 +0700

    Combine Security Group to EC2

--- exercise1/modules/EC2/main.tf
+++ exercise1/modules/EC2/main.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "public" {
   ami = var.ami
   instance_type = var.instance_type
   subnet_id = var.public_subnet_id
-  #security_groups = [var.public_security_group]
+  security_groups = [var.public_security_group]
 
   tags = {
     Name = "Public Instance Group 6"
@@ -13,7 +13,7 @@ resource "aws_instance" "private" {
   ami = var.ami
   instance_type = var.instance_type
   subnet_id = var.private_subnet_id
-  #security_groups = [var.private_security_group]
+  security_groups = [var.private_security_group]
 
   tags = {
     Name = "Private Instance Groub 6"

2427d2707be95edf792528ea6682679b50264ddd
howtodie123 <nguyenletienphat123123@gmail.com>
2024-10-11 16:58:42 +0700

    write description for NAT

--- exercise1/modules/NAT/output.tf
+++ exercise1/modules/NAT/output.tf
@@ -1,3 +1,4 @@
 output "nat_gateway_id" {
-    value = aws_nat_gateway.nat_gateway.id
+    value       = aws_nat_gateway.nat_gateway.id
+    description = "The ID of the NAT Gateway"
 }
--- exercise1/modules/NAT/variable.tf
+++ exercise1/modules/NAT/variable.tf
@@ -1,4 +1,4 @@
 variable "public_subnet_id" {
-  type = string
-  
+  type        = string
+  description = "The ID of the public subnet where the NAT gateway will be deployed"
 }

54e08a00ffe70fd1ab0d04163976e6f1b6885488
howtodie123 <nguyenletienphat123123@gmail.com>
2024-10-11 11:26:44 +0700

    Create VPC module (main , output , variable)

--- exercise1/main.tf
+++ exercise1/main.tf
@@ -13,7 +13,15 @@ provider "aws" {
   secret_key = ""
 }
 
-resource "aws_instance" "Group6" {
-  ami = "ami-005fc0f236362e99f" # this is the ami id for the ubuntu 20.04
-  instance_type = "t2.micro" # this is the instance type
+# resource "aws_instance" "Group6" {
+#   ami = "ami-005fc0f236362e99f" # this is the ami id for the ubuntu 20.04
+#   instance_type = "t2.micro" # this is the instance type
+# }
+
+module "vpc" {
+  source                  = "./module/VPC"
+  vpc_cidr                = "10.0.0.0/16"
+  public_subnet_cidr      = "10.0.1.0/24"
+  private_subnet_cidr     = "10.0.2.0/24"
+  availability_zone       = "us-east-1a"
 }

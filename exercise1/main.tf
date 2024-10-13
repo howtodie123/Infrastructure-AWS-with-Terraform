8517d0e1d2665efa0105de4f1576076d7c97bdab
howtodie123 <nguyenletienphat123123@gmail.com>
2024-10-13 19:01:42 +0700

    fix code

--- exercise1/main.tf
+++ exercise1/main.tf
@@ -1,11 +1,11 @@
-# terraform {
-#   required_providers {
-#     aws = {
-#       source = "hashicorp/aws"
-#       version = "5.70.0"
-#     }
-#   }
-# }
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.70.0"
+    }
+  }
+}
 
 provider "aws" {
   region = "us-east-1" 

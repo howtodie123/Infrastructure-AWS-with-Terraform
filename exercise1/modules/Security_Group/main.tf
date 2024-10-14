696deb54d6a58a84055a42561e71c0dd3553d0eb
howtodie123 <nguyenletienphat123123@gmail.com>
2024-10-14 20:39:02 +0700

    destroy terraform

--- exercise1/modules/Security_Group/main.tf
+++ exercise1/modules/Security_Group/main.tf
@@ -1,5 +1,7 @@
 resource "aws_default_security_group" "default_security_group_7" {
   vpc_id = var.vpc_id
+  name = "Group 7: Private Security Group EC2"
+  
   tags = {
     Name = "Group 7: Default Security Group EC2"
   }
@@ -8,7 +10,8 @@ resource "aws_default_security_group" "default_security_group_7" {
 resource "aws_security_group" "public" {
   vpc_id = var.vpc_id
   description = "Allow SSH access from a specific IP"
-  
+  name = "Group 7: Public Security Group EC2"
+
   # Allow SSH from a specific IP 
   ingress {
     description = "Allow SSH from a specific IP"
@@ -33,7 +36,8 @@ resource "aws_security_group" "public" {
 
 resource "aws_security_group" "private" {
   vpc_id = var.vpc_id
-  description = "Allow connections from Public EC2 instance"
+  description = "Allow connections from Private EC2 instance"
+  name = "Group 7: Private Security Group EC2"
 
   ingress {
     description = "Allow SSH from a specific IP"

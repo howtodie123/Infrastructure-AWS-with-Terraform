0327fd8d809dd2ee8887dd6f656a9f6944808e18
howtodie123 <nguyenletienphat123123@gmail.com>
2024-10-14 20:23:03 +0700

    fix key backend

--- exercise1/main.tf
+++ exercise1/main.tf
@@ -16,7 +16,7 @@ provider "aws" {
 terraform {
   backend "s3" {
     bucket =  "group7-terraform-bucket"       # name of the S3 bucket
-    #key    = "dev/terraform.tfstate"         # path to the state file inside the bucket
+    key    = "dev/terraform.tfstate"         # path to the state file inside the bucket
     region =  "us-east-1"                     # region of the S3 bucket
     encrypt = true                           # enable server-side encryption
   }
